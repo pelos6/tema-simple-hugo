6ac19ea19bac3642846b1d56e6a5d77e7ad36017
javier <javieriranzo3@gmail.com>
2018-09-19 07:59:36 +0200

    calendar

--- static/js/caption.js
+++ static/js/caption.js
@@ -1,3 +1,32 @@
 
 
 
+$(document).ready(
+
+  function(){
+
+    // Comprobar si estamos, al menos, 100 px por debajo de la posición top
+    // para mostrar o esconder el botón
+    $(window).scroll(function(){
+
+      if ( $(this).scrollTop() > 100 ) {
+
+        $('.arriba-link').fadeIn();
+
+      } else {
+
+        $('.arriba-link').fadeOut();
+
+      }
+
+    });
+
+    // al hacer click, animar el scroll hacia arriba
+    $('.arriba-link').click( function( e ) {
+
+      e.preventDefault();
+      $('html, body').animate( {scrollTop : 0}, 800 );
+
+    });
+
+  });
